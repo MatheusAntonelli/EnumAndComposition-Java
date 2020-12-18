aef86d02a80d340d090db1a31102e78a2440d783
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2020-12-18 15:50:09 -0300

    Create Department.java and HourContract.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,11 @@
 package application;
 
-import entities.Order;
-import entities.enums.OrderStatus;
 
-import java.util.Date;
+
 
 public class Program {
     public static void main(String[] args) {
 
-        Order order = new Order(1070, new Date(), OrderStatus.PENDING_PAYMENT);
 
-        System.out.println(order);
     }
 }
